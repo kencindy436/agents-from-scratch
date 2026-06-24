9ca7b01e5eef05dde388c73232f8758bcb5dbe46
xiaoqi <kencindy436@gmail.com>
2026-06-24 21:34:58 +0800

    lesson2 加入系统提示词的使用

--- lesson_02_practice.py
+++ lesson_02_practice.py
@@ -22,11 +22,24 @@ class LocalLLM:
 class SimpleAgent:
     def __init__(self, model_path):
         self.llm = LocalLLM(model_path)
-
+        self.system_prompt = (
+            "你是一个中文 AI 教学助手。"
+            "你只使用中文回答。"
+            "你用简单、清楚的语言解释概念。"
+            "不要提供英文翻译。" 
+        )
     def simple_generate(self, user_input):
         return self.llm.generate(user_input)
 
+    def generate_with_role(self,user_input):
+        prompt = f"""{self.system_prompt}
+
+User: {user_input}
+Assitant:"""
+
+        response = self.llm.generate(prompt)
+        return response.strip()
 
 agent = SimpleAgent("models/llama-3-8b-instruct.gguf")
-response = agent.simple_generate("Explain what an AI agent is in one sentence.")
+response = agent.generate_with_role("Explain what an AI agent is in one sentence.")
 print(response)
