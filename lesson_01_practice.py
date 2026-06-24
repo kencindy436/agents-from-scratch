ab88acefc8d080cffb8f6402f3f6b148da087da2
xiaoqi <kencindy436@gmail.com>
2026-06-24 20:28:31 +0800

    llm打包成agent

--- lesson_01_practice.py
+++ lesson_01_practice.py
@@ -1,25 +1,25 @@
+
+#llm打包成agent
+
 from llama_cpp import Llama
 
-model = Llama(
-    model_path="models/llama-3-8b-instruct.gguf",
-    n_ctx=2048,
-    verbose=False,
-)
+class SimpleAgent:
+    def __init__(self,model_path):
+        self.model = Llama (
+            model_path=model_path,
+            n_ctx=2048,
+            verbose=False,
+        )
+    def simple_generate(self,user_input):
+        response = self.model(
+            prompt=user_input,
+            max_tokens=128,
+            stop=["</s>", "\n\n", "User:", "Assistant:"],
+        )
 
-response = model(
-    prompt= "用一句话解释什么是 AI agent。",
-    max_tokens=128,
-    stop=["</s>","\n\n","User:","Assistant:"],
-)
+        return response["choices"][0]["text"].strip()
 
-"""
-choices = response["choices"]
-first_choice = choices[0]
-text = first_choice["text"]
-clean_text = text.strip()
+agent = SimpleAgent("models/llama-3-8b-instruct.gguf")
+response = agent.simple_generate("Explain what an AI agent is in one sentence.")
 
-print(clean_text) 
-
-"""
-
-print(response["choices"][0]["text"].strip())
+print(response)
